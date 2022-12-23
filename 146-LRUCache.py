447f31335ca64699bd7f0a6c3488c9dc81034cab
divya-rajput <divya.rajput8220@gmail.com>
2022-12-23 12:15:44 -0800

    146. LRU Cache- Adding definition to the comment

--- 146-LRUCache.py
+++ 146-LRUCache.py
@@ -1,8 +1,10 @@
 '''
     This is a Least Recently Used(LRU) problem.
+    Definition: An LRU cache is an efficient cache data structure that can be used to figure out what we should evict when the cache is full. The goal is to always have the least-recently used item accessible in O(1) time. 
     LRU will help you to quickly identify which item hasn't been used for longest amount if time.
     LRU is often implemented by pairing a doubly link list with a hash map
     Strength:  1. Super fast access with O(1) time
                2. Super fast update with O(1) time
     Weaknesses: Space heavy : Contains alinked list with n length and a hash map holting n items.So, O(n) space
 '''
+
