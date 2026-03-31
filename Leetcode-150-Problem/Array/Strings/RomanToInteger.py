a54203aa58a77785f992ec33424f61af2d31b6f4
divya-rajput <divya.rajput8220@gmail.com>
2026-03-30 20:45:46 -0600

    Roman-Integer Problem

---
+++ Leetcode-150-Problem/Array/Strings/RomanToInteger.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_to_integer = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        res = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and roman_to_integer[s[i]] < roman_to_integer[s[i+1]]:
+                res -= roman_to_integer[s[i]]
+            else:
+                res += roman_to_integer[s[i]]
+        return res
+        
