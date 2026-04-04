ef982497ff38f90e1d3f0e78f75313f73a3209ff
divya-rajput <divya.rajput8220@gmail.com>
2026-04-03 23:32:23 -0600

    Problem- add strings

---
+++ DSA sample problems/Strings/AddStrings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        #Problem: https://leetcode.com/problems/add-strings/description/?envType=problem-list-v2&envId=string
+        i, j = len(num1) - 1, len(num2) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            digit1 = int(num1[i]) if i >= 0 else 0
+            digit2 = int(num2[j]) if j >= 0 else 0
+
+            total = digit1 + digit2 + carry
+            carry = total // 10
+
+            result.append(str(total % 10))
+
+            i -= 1
+            j -= 1
+
+        return ''.join(result[::-1])
+        
